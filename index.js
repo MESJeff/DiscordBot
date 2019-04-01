4391a4f03d96b0bc71e502db0dedec58af73fe53
Jeff Underwood <jeff@mutantstudios.com>
2019-04-01 15:49:33 -0700

    force online update2

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const Discord = require('discord.js')
-const client = new Discord.Client()
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log("Connected as "+client.user.tag)
+    console.log('I am ready!');
 });
 
-client.on('message', message =>{
-    if(message.content === 'ping'){
-        message.reply('pong');
-    }
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
 });
 
-
-client.login(process.env.BOT_TOKEN)
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
