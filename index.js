99df7ad1c3d477b4927fe485e1259c86b499db5f
Jeff Underwood <jeff@mutantstudios.com>
2019-04-05 14:29:41 -0700

    random

--- index.js
+++ index.js
@@ -4,17 +4,8 @@ const Discord = require('discord.js');
 // Create an instance of a Discord client
 const client = new Discord.Client();
 
-/**
- * The ready event is vital, it means that only _after_ this will your bot start reacting to information
- * received from Discord
- * 
- * 
- * 
- * 
- * 
- */
 client.on('ready', () => {
-  console.log('I am ready!');
+  console.log('Bot Ready');
 });
 
 // Create an event listener for messages
@@ -22,53 +13,14 @@ client.on('message', message => {
   // If the message is "ping"
   if (message.content === '!loot') {
     // Send "pong" to the same channel
-    message.channel.send('box');
+    message.channel.send('roll: '+random(0, 99));
   }
 });
-
-// Log our bot in using the token from https://discordapp.com/developers/applications/me
-//client.login("1ti6kOhA8BQnLICvM35A6uCPVGjeSnDH");
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
 
-/*
-var Discord = require('discord.io');
-var logger = require('winston');
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(logger.transports.Console, {
-    colorize: true
-});
-logger.level = 'debug';
-// Initialize Discord Bot
-var bot = new Discord.Client({
-   token: process.env.BOT_TOKEN,
-   autorun: true
-});
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
-});
-bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-       
-        args = args.splice(1);
-        switch(cmd) {
-            // !ping
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
-            break;
-            // Just add any case commands if you want to..
-         }
-     }
-});
-*/
+//generate float between low (inclusive) and high (exclusive)
+function random(low, high){
+    return Math.random() * (high-low)+low;
+};
