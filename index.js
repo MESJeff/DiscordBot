91a6835c3d8b887e7d631ae7de05b44918752799
Jeff Underwood <jeff@mutantstudios.com>
2019-04-08 11:31:55 -0700

    still debugging3

--- index.js
+++ index.js
@@ -17,7 +17,11 @@ client.on('message', message =>{
     }
     */
    if(message.content === '!loot'){
-       message.channel.send('box');
+       //message.channel.send('box');
+       var roll = random(0,99);
+        var tier = randomInt(1,7);
+        var type = randomInt(1,5);
+        message.channel.send('roll: '+roll+' tier: '+tier+' type: '+type);
    }
 });
 
